d9a636149732e02da77a0f6a2bca3d1f97e22bac
Chengluo1 <50608316+Chengluo1@users.noreply.github.com>
2019-07-21 18:21:16 +0800

    Create 11hello.py
    我的第一个py代码

---
+++ 11hello.py
@@ -0,0 +1,19 @@
+i=10
+print(i)
+
+print('hello world!')
+
+import turtle
+
+turtle.pensize(4)
+turtle.pencolor('red')
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.right(90)
+turtle.forward(100)
+turtle.mainloop()
+
+import this
